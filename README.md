d24dc4a81cf44e7c4fff126395f69386f250cdba
Siddharth Verma <siddharthv93@gmail.com>
2015-06-12 01:19:22 +0530

    Restructure complete

---
+++ README.md
@@ -0,0 +1,1 @@
+Merlin is a command line tool that recommends movies you based on whatever you tell him. It uses the TMDb API and is build using the Click library. It is written in python 2.7.
--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
         name = 'merlin',
         version = '0.1',
-        py_modules = ['merlin'],
+        description = 'A personal movie recommender',
+        author = 'Siddharth Verma',
+        author_email = 'siddharthv93@gmail.com',
+        url = 'https://github.com/padfoot27/merlin',
+        download_url = 'https://github.com/padfoot27/merlin/tarball/0.1',
+        packages=find_packages(),
+        include_package_data=True,
         install_requires = [
             'Click',
             'tmdbsimple',
         ],
         entry_points = '''
             [console_scripts]
-            merlin=merlin:discover
+            merlin=package.scripts.merlin:discover
         ''',
 )

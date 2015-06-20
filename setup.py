024ecc24be8e93058a72c4a3d852685fc1a46334
Siddharth Verma <siddharthv93@gmail.com>
2015-06-20 18:45:17 +0530

    Add changes to setup file

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup,find_packages
 setup(
         name = 'movieMerlin',
         packages=find_packages(),
-        version = '0.1',
-        description = 'A personal movie recommender',
+        version = '0.2',
+        description = 'A command line tool that finds you movies',
         author = 'Siddharth Verma',
         author_email = 'siddharthv93@gmail.com',
         url = 'https://github.com/padfoot27/merlin',
-        download_url = 'https://github.com/padfoot27/merlin/tarball/0.1',
+        download_url = 'https://github.com/padfoot27/merlin/tarball/0.2',
         include_package_data=True,
+        license = 'MIT License',
         install_requires = [
             'Click',
             'tmdbsimple',
